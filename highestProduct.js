cdf351c8d4d1278310c75596e4eecd0e3136e38c
XAVA | Neill van Steenpaal <104568148+xava-dev@users.noreply.github.com>
2022-09-27 08:56:36 +0200

    Update highestProduct.js

--- highestProduct.js
+++ highestProduct.js
@@ -51,23 +51,53 @@ highestProductApproach1
   : console.log("Approach 1: Array consists of less than three numbers");
 
 
-
 // Approach 2
 
-// Time Complexity: O(n log(n))
-// Space Complexity: O(n)
+// Time Complexity: O(n + k)
+// Space Complexity: O(k)
 
-const calcHighestProductApproach2 = (array) => {
+const calcHighestProductApproach2 = (array, n = array.length) => {
   // Check if array length is less than 3
-  if (array.length < 3) {
+  if (n < 3) {
     return undefined;
   }
 
-  // Return highest product
-  return [...array]
-    .sort((a, b) => a - b)
-    .slice(-3)
-    .reduce((a, b) => a * b, 1);
+  // Calc maximum element
+  let k = Math.max(...array);
+  let t;
+
+  // Create a temporary with 0 zero value
+  // as the same length of max element + 1
+  const temp = new Array(k + 1).fill(0);
+
+  // Count the frequency of each element in the original array
+  // And store it in temporary array
+  for(let i = 0; i < n; i++){
+    t = array[i];
+    temp[t]++;
+  }
+
+  // Update the count based on the previous index
+  for(let i = 1; i <= k; i++){
+    // Updating elements of count array
+    temp[i] = temp[i] + temp[i - 1];
+  }
+
+  //Output arr
+  const outputArray = new Array(n).fill(0);
+
+  for(let i = n - 1; i >= 0; i--) {
+    // Add elements of array A to array B
+    t = array[i];
+    outputArray[temp[t] - 1] = t;
+
+    // Decrement the count value by 1
+    temp[t] = temp[t] - 1;
+  }
+
+  return outputArray
+      .slice(-3)
+      .reduce((a, b) => a * b, 1);
 };
 
 // Save highest product
